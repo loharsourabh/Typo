438f740020771c106a7b26c290947a000160939d
loharsourabh <lohar1024s@gmail.com>
2021-10-06 23:50:57 +0530

    using hash routing

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,13 @@ import Header from './Header/Header.js';
 import Typing from './Typing/Typing.js';
 import Setup from './Setup/Setup.js';
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {HashRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
 
     return (
         <div className='app'>
-            <BrowserRouter>
+            <HashRouter>
                 <Header />
                 
                 <Switch>
@@ -21,7 +21,7 @@ function App() {
                         <Setup />
                     </Route>
                 </Switch>
-            </BrowserRouter>
+            </HashRouter>
         </div>
     );
 }
