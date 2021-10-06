a7f8534e0fc8c4255832e0d8d1f13bc95e8c36ea
loharsourabh <lohar1024s@gmail.com>
2021-10-06 23:54:07 +0530

    reverted to BrowserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,13 @@ import Header from './Header/Header.js';
 import Typing from './Typing/Typing.js';
 import Setup from './Setup/Setup.js';
 import React from 'react';
-import {HashRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
 
     return (
         <div className='app'>
-            <HashRouter>
+            <BrowserRouter>
                 <Header />
                 
                 <Switch>
@@ -21,7 +21,7 @@ function App() {
                         <Setup />
                     </Route>
                 </Switch>
-            </HashRouter>
+            </BrowserRouter>
         </div>
     );
 }
